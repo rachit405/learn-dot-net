306e5a3834a54c1208e5e55b23abf8f109f18bff
rachit405 <51585173+rachit405@users.noreply.github.com>
2023-12-20 13:30:38 +0530

    Added files from local

---
+++ RabbitMQTest/RabbitReciever2/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Reciever2 App";
+
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+    var body = args.Body.ToArray();
+    string msg = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message Recieved : {msg}");
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+var consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
+
